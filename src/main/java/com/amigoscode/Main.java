2c6c09f3e18a349ab36530d0be20132d47d7070b
prista <games.prista@mail.ru>
2024-09-06 16:09:17 +0300

    #record

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -17,5 +17,33 @@ public class Main {
         return new GreetResponse("Hello!");
     }
 
-    record GreetResponse(String greet) {}
+    record GreetResponse(String greet) {
+    }
+
+//    class GreetResponse {
+//        private final String greet;
+//        public GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//        public String getGreet() {
+//            return greet;
+//        }
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//        @Override
+//        public boolean equals(final Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            final GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//        @Override
+//        public int hashCode() {
+//            return Objects.hashCode(greet);
+//        }
+//    }
 }
