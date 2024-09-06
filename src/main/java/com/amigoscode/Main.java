97c95053129a3fec920c2712f32e7dee981ab99c
prista <games.prista@mail.ru>
2024-09-06 16:31:35 +0300

    #Jackson >> transform our Java objects into JSON object

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -14,36 +16,18 @@ public class Main {
 
     @GetMapping("/")
     public GreetResponse greet() {
-        return new GreetResponse("Hello!");
+        final GreetResponse response = new GreetResponse(
+                "Hello",
+                List.of("Java", "Golang", "Javascript"),
+                new Person("Hello!", 28, 30_000));
+        return response;
     }
 
-    record GreetResponse(String greet) {
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person) {
     }
 
-//    class GreetResponse {
-//        private final String greet;
-//        public GreetResponse(String greet) {
-//            this.greet = greet;
-//        }
-//        public String getGreet() {
-//            return greet;
-//        }
-//        @Override
-//        public String toString() {
-//            return "GreetResponse{" +
-//                    "greet='" + greet + '\'' +
-//                    '}';
-//        }
-//        @Override
-//        public boolean equals(final Object o) {
-//            if (this == o) return true;
-//            if (o == null || getClass() != o.getClass()) return false;
-//            final GreetResponse that = (GreetResponse) o;
-//            return Objects.equals(greet, that.greet);
-//        }
-//        @Override
-//        public int hashCode() {
-//            return Objects.hashCode(greet);
-//        }
-//    }
+    record Person(String name, int age, double savings) {}
 }
