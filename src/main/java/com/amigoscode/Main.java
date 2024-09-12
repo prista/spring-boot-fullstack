fcb8c74130da48334ecdf03426cd210f2eaaeb99
prista <games.prista@mail.ru>
2024-09-12 13:14:41 +0300

    #REST CRUD, initial

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -2,35 +2,100 @@ package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
 public class Main {
+
+    private static List<Customer> customers;
+
+    static {
+        // initialize our DB
+        customers = new ArrayList<>();
+        Customer alex = new Customer(1, "Alex", "alex@gmail.com", 21);
+        Customer jamila = new Customer(2, "Jamila", "jamila@gmail.com", 19);
+        customers.add(alex);
+        customers.add(jamila);
+    }
+
     public static void main(String[] args) {
+        System.out.println(customers);
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/")
-    public GreetResponse greet(@RequestParam(
-            value = "name", required = false) String name) {
-        String greetMessage = name == null || name.isBlank() ? "Hello" : "Hello " + name;
-        final GreetResponse response = new GreetResponse(
-                greetMessage,
-                List.of("Java", "Golang", "Javascript"),
-                new Person("Hello!", 28, 30_000));
-        return response;
-    }
+    static class Customer {
+        private Integer id;
+        private String name;
+        private String email;
+        private Integer age;
 
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person) {
-    }
+        public Customer() {}
 
-    record Person(String name, int age, double savings) {}
+        public Customer(final Integer id, final String name, final String email, final Integer age) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.age = age;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(final Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(final String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(final String email) {
+            this.email = email;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(final Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            final Customer customer = (Customer) o;
+            return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, email, age);
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
 }
