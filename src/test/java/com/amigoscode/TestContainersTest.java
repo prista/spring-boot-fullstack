c1b999156fa472453db31673026a59d7af9080f5
prista <games.prista@mail.ru>
2024-09-30 22:48:36 +0300

    #apply migrations with Flyway and Testcontainers

--- src/test/java/com/amigoscode/TestContainersTest.java
+++ src/test/java/com/amigoscode/TestContainersTest.java
@@ -1,5 +1,6 @@
 package com.amigoscode;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -21,5 +22,16 @@ public class TestContainersTest {
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+
+    @Test
+    void canApplyDbMigrationsWithFlyway() {
+        var flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
+        System.out.println();
     }
 }
