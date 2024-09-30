5f49bd048ba8abb6da92f79448320b2f453ab059
prista <games.prista@mail.ru>
2024-09-30 17:44:47 +0300

    #BACK TO 3.0.0 SPRING BOOT. TestContainers dependencies

---
+++ src/test/java/com/amigoscode/TestContainersTest.java
@@ -0,0 +1,25 @@
+package com.amigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestContainersTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("amigoscode-dao-unit-test")
+                    .withUsername("amigoscode")
+                    .withPassword("password");
+
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+}
