607fdf0bd312973f59bcdeb21e7e072effa2a9f6
prista <games.prista@mail.ru>
2024-10-01 16:47:38 +0300

    Configure JDBC Template for tests

--- src/test/java/com/amigoscode/AbstractTestcontainers.java
+++ src/test/java/com/amigoscode/AbstractTestcontainers.java
@@ -2,11 +2,15 @@ package com.amigoscode;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
 
 @Testcontainers
 public abstract class AbstractTestcontainers {
@@ -44,4 +48,16 @@ public abstract class AbstractTestcontainers {
         );
     }
 
+    private static DataSource getDataSource() {
+        var builder = DataSourceBuilder.create()
+                .driverClassName(postgreSQLContainer.getDriverClassName())
+                .url(postgreSQLContainer.getJdbcUrl())
+                .username(postgreSQLContainer.getUsername())
+                .password(postgreSQLContainer.getPassword());
+        return builder.build();
+    }
+
+    protected static JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(getDataSource());
+    }
 }
---
+++ src/test/java/com/amigoscode/customer/CustomerJDBCDataAccessServiceTest.java
@@ -0,0 +1,47 @@
+package com.amigoscode.customer;
+
+import com.amigoscode.AbstractTestcontainers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomerJDBCDataAccessServiceTest extends AbstractTestcontainers {
+
+    private CustomerJDBCDataAccessService underTest;
+    private final CustomerRowMapper customerRowMapper = new CustomerRowMapper();
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CustomerJDBCDataAccessService(
+                getJdbcTemplate(),
+                customerRowMapper
+        );
+    }
+
+    @Test
+    void selectAllCustomers() {
+    }
+
+    @Test
+    void selectCustomerById() {
+    }
+
+    @Test
+    void insertCustomer() {
+    }
+
+    @Test
+    void existsPersonWithEmail() {
+    }
+
+    @Test
+    void existsPersonWithId() {
+    }
+
+    @Test
+    void deleteCustomerById() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+}
