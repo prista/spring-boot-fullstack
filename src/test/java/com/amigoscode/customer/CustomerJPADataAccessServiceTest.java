7f5d918faf6eb79f01cbba8f0b23d7d31d2579a9
prista <games.prista@mail.ru>
2024-10-08 18:32:21 +0300

    CustomerJPADataAccessServiceTest, part 2

--- src/test/java/com/amigoscode/customer/CustomerJPADataAccessServiceTest.java
+++ src/test/java/com/amigoscode/customer/CustomerJPADataAccessServiceTest.java
@@ -37,25 +37,69 @@ class CustomerJPADataAccessServiceTest {
 
     @Test
     void selectCustomerById() {
+        // Given
+        var id = 1L;
+        // When
+        underTest.selectCustomerById(id);
+        // Then
+        verify(customerRepository).findById(id);
     }
 
     @Test
     void insertCustomer() {
+        // Given
+        var Customer = new Customer(
+                "Ali",
+                "ali@gmail.com",
+                20
+        );
+        // When
+        underTest.insertCustomer(Customer);
+        // Then
+        verify(customerRepository).save(Customer);
     }
 
     @Test
     void existsPersonWithEmail() {
+        // Given
+        var email = "foo@gmail.com";
+        // When
+        underTest.existsPersonWithEmail(email);
+        // Then
+        verify(customerRepository).existsCustomerByEmail(email);
     }
 
     @Test
     void existsPersonWithId() {
+        // Given
+        var id = 1L;
+        // When
+        underTest.existsPersonWithId(id);
+        // Then
+        verify(customerRepository).existsCustomerById(id);
     }
 
     @Test
     void deleteCustomerById() {
+        // Given
+        var id = 1L;
+        // When
+        underTest.deleteCustomerById(id);
+        // Then
+        verify(customerRepository).deleteById(id);
     }
 
     @Test
     void updateCustomer() {
+        // Given
+        var customer = new Customer(
+                "Ali",
+                "ali@gmail.com",
+                20
+        );
+        // When
+        underTest.updateCustomer(customer);
+        // Then
+        verify(customerRepository).save(customer);
     }
 }
