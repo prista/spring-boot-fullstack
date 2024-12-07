6c64b19ee0e3e5f642d503a92f222f618dbc5941
prista <games.prista@mail.ru>
2024-12-07 14:20:46 +0300

    #check 7

--- backend/src/main/java/com/amigoscode/Main.java
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -14,7 +14,7 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        // test 6 with username
+        // test 7 with pwd
         SpringApplication.run(Main.class, args);
     }
 
